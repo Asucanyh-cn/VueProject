877e5b81714a8e6fe7d5b49d52100903b5e7345b
asucanyh-cn <asucanyh@outlook.com>
2023-07-27 15:19:23 +0800

    clean-webpack-plugin和source map的 配置和使用。

--- 01-奇偶行变色/src/index1.js
+++ 01-奇偶行变色/src/index1.js
@@ -1,5 +1,9 @@
 import $ from 'jquery'
 import './css/index.css'
+import logo from './images/nodejs.png'
+
+$('.logo').attr('src', logo);
+console.log(logo);
 
 ///////////babel-loader////////////
 //定义修饰器
@@ -7,10 +11,10 @@ function info(target) {
     target.info = 'Person info';
 }
 //定义普通类
-@info 
-class Person {}
+@info
+class Person { }
 //打印普通类中的属性info的值
-console.log(Person.info);
+consol.log(Person.info);
 ////////////////////////////////////
 $(function () {
     $('li:even').css('background-color', 'pink');
--- 01-奇偶行变色/webpack.config.js
+++ 01-奇偶行变色/webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+//1.导入CleanWebpack插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//2.创建CleanWebpack对象
+const cleanWebpackPlugin = new CleanWebpackPlugin();
+
 //1.导入HTML插件
 const HtmlPlugin = require('html-webpack-plugin');
 //2.创建HTML对象
@@ -6,16 +11,16 @@ const htmlPlugin = new HtmlPlugin({
     template: './src/index.html', //指定源文件的位置
     filename: './index.html' //指定目标文件的位置
 });
-//3.通过plugins节点，使htmlPlugin实例生效
-
+//3.通过plugins节点，使新建的实例生效
 module.exports = {
+    devtool:'nosources-source-map',
     entry: path.join(__dirname, './src/index1.js'),
     output: {
         path: path.join(__dirname, './dist'),
-        filename: 'main1.js'
+        filename: 'js/main1.js'
     },
     mode: 'development', //用于指定构建模式，可选值：development、production
-    plugins: [htmlPlugin],//加载并调用插件
+    plugins: [htmlPlugin, cleanWebpackPlugin],//加载并调用插件
     devServer: {
         open: true,
         port: 8081,
@@ -23,8 +28,9 @@ module.exports = {
     },
     module: {//所有使用第三方文件模块的匹配规则
         rules: [ //文件后缀名匹配规则
-            { test: /\.css$/,use:['style-loader','css-loader']},
-            {test:/\.js$/,use:['babel-loader'],exclude:/node_modules/}//注意必须排除node_modules目录。
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.jpg|png|gif$/, use: 'url-loader?limit=889&outputPath=images' },
+            { test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/ }//注意必须排除node_modules目录。
         ]
-    }
+    },
 }
